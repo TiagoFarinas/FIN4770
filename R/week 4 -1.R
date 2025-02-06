843a0beb62f6cedcff2e43f5b0c1906c687bdad6
Tiago Farinas <tiagofarinas@gmail.com>
2025-02-05 21:16:57 -0500

    push

---
+++ R/week 4 -1.R
@@ -0,0 +1,35 @@
+print("Hello, wor")
+"hELLO, world"
+#Produtividade e conteudo educativo
+1/200 * 30
+3>=5
+TRUE == FALSE
+TRUE & FALSE
+(3 > 1) & (3 <= 5)
+TRUE|FALSE
+!TRUE == FALSE
+!(3>1)4(3<=5)
+length(c(2,3)*c(3,9))
+
+#type of data
+a<- TRUE
+b<-3.56
+c<- "titit"
+d<-6
+
+typeof(a)
+typeof(b)
+typeof(c)
+typeof(d)
+
+#list creation example
+# use "=" inside function 
+my_list <- list(first_element= c(1L,2L,3L),
+                second_element = c("how","what", "why"),
+                third_element = c(3<5,3==5,3>5))
+typeof(my_list)
+
+#matrix
+x <- 1:27
+dim(x)<- c(3,3,3)
+print(x)
